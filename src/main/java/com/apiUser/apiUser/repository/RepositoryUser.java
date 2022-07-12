57f25fe9c5f6557105d892e2ff35893258793114
Moises Griott <109049097+mgriott@users.noreply.github.com>
2022-07-12 16:40:17 -0400

    Delete src directory

--- src/main/java/com/apiUser/apiUser/repository/RepositoryUser.java
+++
@@ -1,9 +0,0 @@
-package com.apiUser.apiUser.repository;
-
-import com.apiUser.apiUser.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryUser extends JpaRepository<User, Long> {
-}
